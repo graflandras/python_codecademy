cab9bbba87d46e060ba89388121a5eec681bc666
graflandras <graflandras@gmail.com>
2020-08-13 22:32:20 +0200

    lesson recap

--- 04 - Lists/list_operations.py
+++ 04 - Lists/list_operations.py
@@ -54,3 +54,28 @@ games = ['Portal', 'Minecraft', 'Pacman', 'Tetris', 'The Sims', 'Pokemon']
 games_sorted = sorted(games)
 print(games) #>>['Portal', 'Minecraft', 'Pacman', 'Tetris', 'The Sims', 'Pokemon']
 print(games_sorted) #>>['Minecraft', 'Pacman', 'Pokemon', 'Portal', 'Tetris', 'The Sims']
+
+#Recap
+inventory = ['twin bed', 'twin bed', 'headboard', 'queen bed', 'king bed', 'dresser', 'dresser', 'table', 'table', 'nightstand', 'nightstand', 'king bed', 'king bed', 'twin bed', 'twin bed', 'sheets', 'sheets', 'pillow', 'pillow']
+#inventory is a list of items that are in the warehouse for Bob’s Furniture. How many items are in the warehouse?
+
+#Save your answer to inventory_len.
+inventory_len = len(inventory)
+
+#Select the first element in inventory. Save it to the variable first.
+first = inventory[0]
+
+#Select the last item from inventory and save it to the variable last.
+last = inventory[-1]
+
+#Select items from the inventory starting at index 2 and up to, but not including, index 6. Save your answer to inventory_2_6.
+inventory_2_6 = inventory[2:6]
+
+#Select the first 3 items of inventory and save it to the variable first_3.
+first_3 = inventory[:3]
+
+#How many 'twin bed's are in inventory? Save your answer to twin_beds.
+twin_beds = inventory.count('twin bed')
+
+#Sort inventory using .sort().
+inventory.sort()

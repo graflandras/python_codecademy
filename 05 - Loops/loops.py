2aa2d47c8704b2f1b40a07f907556f767aa2bbbf
graflandras <graflandras@gmail.com>
2020-08-15 19:09:17 +0200

    range in loops

--- 05 - Loops/loops.py
+++ 05 - Loops/loops.py
@@ -2,3 +2,7 @@ dog_breeds = ['french_bulldog', 'dalmatian', 'shihtzu', 'poodle', 'collie']
 
 for breed in dog_breeds:
     print(breed)
+
+#For example, if we wanted to print out a "WARNING!" message three times, we would want to say something like:
+for i in range(3):
+  print("WARNING!")

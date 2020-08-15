7e581a93add765aadd49b4c3114d8d8ad07894c2
graflandras <graflandras@gmail.com>
2020-08-15 19:29:42 +0200

    nested loops

--- 05 - Loops/loops.py
+++ 05 - Loops/loops.py
@@ -34,11 +34,35 @@ for i in big_number_list:
 # You are adding students to a Poetry class, the size of which is capped at 6.
 # While the length of the students_in_poetry list is less than 6, use .pop()
 # to take a student off the all_students list and add it to the students_in_poetry list.
-all_students = ["Alex", "Briana", "Cheri", "Daniele", "Dora", "Minerva", "Alexa", "Obie", "Arius", "Loki"]
+all_students = ["Alex", "Briana", "Cheri", "Daniele",
+                "Dora", "Minerva", "Alexa", "Obie", "Arius", "Loki"]
 students_in_poetry = []
 
 while len(students_in_poetry) < 6:
-  student = all_students.pop()
-  students_in_poetry.append(student)
-  
+    student = all_students.pop()
+    students_in_poetry.append(student)
+
 print(students_in_poetry)
+
+
+# Nested Loops
+# What if we have a list made up of multiple lists? How can we loop through all of the individual elements?
+# Suppose we are in charge of a science class, that is split into three project teams:
+
+project_teams = [["Ava", "Samantha", "James"],
+                 ["Lucille", "Zed"], ["Edgar", "Gabriel"]]
+
+# If we want to go through each student, we have to put one loop inside another:
+for team in project_teams:
+    for student in team:
+        print(student)
+
+sales_data = [[12, 17, 22], [2, 10, 3], [5, 12, 13]]
+
+scoops_sold = 0
+for location in sales_data:
+    print(location)
+    for number in location:
+        scoops_sold += number
+
+print(scoops_sold)

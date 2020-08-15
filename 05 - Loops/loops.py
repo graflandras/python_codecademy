42e90d3f410d7ab7299fd603ecfcc809d271a83b
graflandras <graflandras@gmail.com>
2020-08-15 19:19:01 +0200

    continue

--- 05 - Loops/loops.py
+++ 05 - Loops/loops.py
@@ -17,3 +17,14 @@ for dog in dog_breeds_available_for_adoption:
     if (dog == dog_breed_I_want):
         print("They have the dog I want!")
         break
+
+
+# When we’re iterating through lists, we may want to skip some values.
+# Let’s say we want to print out all of the numbers in a list, unless they’re negative.
+# We can use continue to move to the next i in the list:
+big_number_list = [1, 2, -1, 4, -5, 5, 2, -9]
+
+for i in big_number_list:
+    if i < 0:
+        continue
+    print(i)

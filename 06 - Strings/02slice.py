2ff0d23f8678409c7e18ed3d7a95539d3b248e4c
graflandras <graflandras@gmail.com>
2020-08-16 14:43:37 +0200

    string slice

---
+++ 06 - Strings/02slice.py
@@ -0,0 +1,9 @@
+# string_name[first_index:last_index]
+# When we slice a string we are creating a new string that starts at
+# (and includes) the first_index and ends at (but excludes) the last_index.
+
+first_name = "Rodrigo"
+last_name = "Villanueva"
+
+new_account = last_name[:5]
+temp_password = last_name[2:6]

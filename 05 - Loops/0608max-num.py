440e041aa3289cd4d6861ca6c007daa667b03af2
graflandras <graflandras@gmail.com>
2020-08-16 14:16:10 +0200

    max num

---
+++ 05 - Loops/0608max-num.py
@@ -0,0 +1,9 @@
+# Create a function named max_num() that takes a list of numbers named nums as a parameter.
+# The function should return the largest number in nums
+
+def max_num(nums):
+    return max(nums)
+
+
+# Uncomment the line below when your function is done
+print(max_num([50, -10, 0, 75, 20]))

b04df101ccf9496ec8ed06759826bbf11ab6139e
graflandras <graflandras@gmail.com>
2020-08-18 13:52:50 +0200

    joining strings 1

---
+++ 06 - Strings/16joining-strings.py
@@ -0,0 +1,8 @@
+# 'delimiter'.join(list_you_want_to_join)
+
+reapers_line_one_words = ["Black", "reapers", "with",
+                          "the", "sound", "of", "steel", "on", "stones"]
+
+reapers_line_one = ' '.join(reapers_line_one_words)
+
+print(reapers_line_one)

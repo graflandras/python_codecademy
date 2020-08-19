88f45fc28ca6f18705222a95c36317bbefddf457
graflandras <graflandras@gmail.com>
2020-08-19 14:22:29 +0200

    find

---
+++ 06 - Strings/20find.py
@@ -0,0 +1,12 @@
+# .find() takes a string as an argument and searching the string it was run on
+# for that string. It then returns the >>>first index value where that string is located.
+
+# >>> 'smooth'.find('o')
+# '2'
+
+god_wills_it_line_one = "The very earth will disown you"
+
+# At what index place does the word “disown” appear?
+# Save that index place to the variable disown_placement.
+
+disown_placement = god_wills_it_line_one.find('disown')

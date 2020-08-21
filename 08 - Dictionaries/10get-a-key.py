50bc9415953e3bc30a0d7c516244b6776e602619
graflandras <graflandras@gmail.com>
2020-08-21 16:01:33 +0200

    10get-a-key.py

---
+++ 08 - Dictionaries/10get-a-key.py
@@ -0,0 +1,9 @@
+# Once you have a dictionary, you can access the values in it by providing the key.
+
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": [
+    "Taurus", "Virgo", "Capricorn"], "air": ["Gemini", "Libra", "Aquarius"]}
+
+# Print out the list of zodiac signs associated with the "earth" element.
+print(zodiac_elements["earth"])
+# Print out the list of the "fire" signs.
+print(zodiac_elements["fire"])

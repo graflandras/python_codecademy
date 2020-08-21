261317f2a7c4c404cf88de71006b92ca91224ec6
graflandras <graflandras@gmail.com>
2020-08-21 16:00:55 +0200

    05add-a-key.py

---
+++ 08 - Dictionaries/05add-a-key.py
@@ -0,0 +1,17 @@
+# To add a single key : value pair to a dictionary, we can use the syntax:
+#my_dict["new_key"] = "new_value"
+
+# Create an empty dictionary called animals_in_zoo.
+animals_in_zoo = {}
+
+# Walking around the zoo, you see 8 zebras. Add "zebras" to animals_in_zoo as a key with a value of 8.
+animals_in_zoo["zebras"] = 8
+
+# The primate house was bananas! Add "monkeys" to animals_in_zoo as a key with a value of 12.
+animals_in_zoo["monkeys"] = 12
+
+# As you leave the zoo, you are saddened that you did not see any dinosaurs. Add "dinosaurs" to animals_in_zoo as a key with a value of 0.
+animals_in_zoo["dinosaurs"] = 0
+
+# Print animals_in_zoo.
+print(animals_in_zoo)

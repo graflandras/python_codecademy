a50ebaadef4b30168a6b9153e45798b03761db4b
graflandras <graflandras@gmail.com>
2020-08-21 16:01:09 +0200

    07overwrite-values.py

---
+++ 08 - Dictionaries/07overwrite-values.py
@@ -0,0 +1,10 @@
+oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck",
+                 "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
+
+# Add the key "Supporting Actress" and set the value to "Viola Davis".
+oscar_winners["Supporting Actress"] = "Viola Davis"
+
+# Without changing the definition of the dictionary oscar_winners, change the value associated with the key "Best Picture" to "Moonlight".
+
+oscar_winners["Best Picture"] = "Moonlight"
+print(oscar_winners)
